dab1c6ff5f760472e1a2f739038673bd7616363a
Justin Cox <justincox@Violmusio.attlocal.net>
2023-03-20 12:44:04 -0500

    Add in-development notice

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,9 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <Header title="Jcodev Portfolio" />
+      <section>
+        <p className={styles.inDevelopment}>This portfolio is currently in development</p>
+      </section>
     </main>
   )
 }
