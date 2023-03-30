68dc89846328c36da92b8a992b36d9812347b0d9
Justin Cox <justincox@Violmusio.attlocal.net>
2023-03-30 12:17:58 -0500

    Add projects component

--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -22,7 +22,16 @@ export default function Projects () {
 
       {/* Projects Listing */}
       <div>
-
+        <ul>
+          <li>Project 1</li>
+          <li>Project 2</li>
+          <li>Project 3</li>
+          <li>Project 4</li>
+          <li>Project 5</li>
+          <li>Project 6</li>
+          <li>Project 7</li>
+          <li>Project 8</li>
+        </ul>
       </div>
     </section>
   )
